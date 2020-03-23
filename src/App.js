ab54ad298160a330c3e7e4d9a91691ebecdd1edb
corinne carew <ccarew@mail.usf.edu>
2020-03-23 17:32:44 -0400

    explorer Mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
-const App = () => {
-  return <HelloWorld />
+class App extends Component {
+  state = {
+    hue: Math.round(Math.random() * 360),
+    saturation: Math.round(Math.random() * 100),
+    light: Math.round(Math.random() * 100),
+    value: Math.round(Math.random() * 100),
+  }
+  changeHue = e => {
+    this.setState({
+      hue: e.target.value,
+    })
+    console.log(e.target.value)
+  }
+  changeSaturation = e => {
+    this.setState({
+      saturation: e.target.value,
+    })
+  }
+  changeLight = e => {
+    this.setState({
+      light: e.target.value,
+    })
+  }
+  render() {
+    const newColor =
+      'hsl (${this.state.hue}, ${this.state.saturation}%, ${this.state.light}%)'
+    console.log(newColor)
+    return (
+      <>
+        <Header />
+        <section>
+          {/* <section class = "slidecontainer" style {{backgroundColor : newColor}}> */}
+          <ul>
+            <li className="H">
+              <h1>h</h1>
+              <input
+                type="range"
+                min="1"
+                max="360"
+                class="slider"
+                onInput={this.changeHue}
+                value={this.state.hue}
+              />
+            </li>
+            <li className="S">
+              <h1>s</h1>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                class="slider"
+                onInput={this.changeSaturation}
+                value={this.state.saturation}
+              />
+            </li>
+            <li className="L">
+              <h1>l</h1>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                onInput={this.changeLight}
+                value={this.state.light}
+              />
+            </li>
+          </ul>
+        </section>
+      </>
+    )
+  }
 }
 
 export default App
